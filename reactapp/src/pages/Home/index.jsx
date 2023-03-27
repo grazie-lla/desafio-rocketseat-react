ac182082b28e04c19a8abcedf08efb41e7c41890
grazie-lla <grazidcosta@gmail.com>
2023-03-26 22:53:22 -0300

    added header

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -24,7 +24,13 @@ export function Home() {
   
    return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>Graziella</strong>
+          <img src='https://github.com/grazie-lla.png' alt='foto de perfil'/>
+        </div>
+      </header>
       <input 
         type="text" 
         placeholder="Digite o nome..."
